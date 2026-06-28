5f526833710d953160bd819a0091a38622268491
Siddheshwari Dilip Sonar <siddheshwaridsonar@gmail.com>
2026-06-28 16:58:19 +0530

    fix: update API baseURL for production

--- room-booking-client/src/services/api.js
+++ room-booking-client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://room-booking-6fvq.onrender.com/api"
 });
 
 API.interceptors.request.use((config) => {
